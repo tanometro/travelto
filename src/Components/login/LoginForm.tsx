ae9b5d4a1a5b4e57bc3403ca4a6f7bc9f8929a1b
ricardo <rblazquez111@gmail.com>
2023-10-22 15:06:16 -0300

    feat(app/Login) add validate form

--- src/Components/login/LoginForm.tsx
+++ src/Components/login/LoginForm.tsx
@@ -1,24 +1,31 @@
 "use client"
 import { useState } from "react"
 import { signIn, useSession } from "next-auth/react"
+import validate from "./validate";
 
 export const LoginForm = () => {
     const { data: session, status } = useSession();
-    //const [errors, setErrors] = useState<string[]>([]);
+    const [errors, setErrors] = useState<string[]>([]);
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+
+
     const handlerSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        //setErrors([]);
-        // aca llamo a la api
-        const responseNextAuth = signIn("credentials", {
-            name,
-            email,
-            password,
-            redirect: false,
-        })
+        const stateErrors: string[] = validate(name, email, password)
+        setErrors([...stateErrors]);
+        if (stateErrors.length === 0) {
+            // aca llamo a la api
+
+            const responseNextAuth = signIn("credentials", {
+                name,
+                email,
+                password,
+                redirect: false,
+            })
+        }
     }
     return (
         <form onSubmit={handlerSubmit}>
@@ -29,7 +36,7 @@ export const LoginForm = () => {
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         value={name}
                         placeholder="Name..."
-                        onChange={(event) => setEmail(event.target.value)} />
+                        onChange={(event) => setName(event.target.value)} />
                 </label>
             </div>
             <div className="flex items-center">
@@ -52,10 +59,17 @@ export const LoginForm = () => {
             <div className="relative mb-6 flex flex-col gap-y-4">
                 <button className="flex w-full justify-center p-30 rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">SING IN</button>
 
-                <button className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => signIn("google", {
+                <button type="button" className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => signIn("google", {
                     redirect: false,
                 })}>Sign In with Google</button>
             </div>
+            {errors.length > 0 && (
+                <div className="text-red-500">
+                    {errors.map((error, index) => (
+                        <p key={index}>{error}</p>
+                    ))}
+                </div>
+            )}
 
         </form>
 
