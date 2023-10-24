d557fc2bd375beb31a83553a5f76f4ec808fff47
michqiueli <michiqueli@gmail.com>
2023-10-24 02:24:48 -0300

    Formularios

--- src/app/AdminDashboard/AdminAttractions/CreateAttraction/page.tsx
+++ src/app/AdminDashboard/AdminAttractions/CreateAttraction/page.tsx
@@ -1,6 +1,19 @@
 "use client";
-import AdminAttractionForm from "@/Components/AdminComponents/Attractions/AdminAttractonForm";
+import AdminAttractionForm from "@/src/Components/AdminComponents/Attractions/AdminAttractonForm";
+
+const initialFormData = {
+  name: "",
+  description: "",
+  latitude: "",
+  longitude: "",
+  price: "",
+  hours: "",
+  duration: "",
+  image: "",
+  isActive: true,
+  location: [],
+};
 
 export default function CreateAttraction() {
-  return <AdminAttractionForm />;
+  return <AdminAttractionForm initialFormData={initialFormData}/>;
 }
