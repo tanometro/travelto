71c5af7cb571e2772c3b28287a545c546389aec4
michiqueli <michiqueli@gmail.com>
2023-10-24 07:59:17 -0300

    arreglos

--- src/app/AdminDashboard/AdminAttractions/CreateAttraction/page.tsx
+++ src/app/AdminDashboard/AdminAttractions/CreateAttraction/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 import AdminAttractionForm from "@/src/Components/AdminComponents/Attractions/AdminAttractonForm";
 
+const initialFormData = {
+  name: "",
+  description: "",
+  latitude: "",
+  longitude: "",
+  price: "",
+  hours: "",
+  duration: "",
+  image: "",
+  isActive: true,
+  location: [],
+};
+
 export default function CreateAttraction() {
   return <AdminAttractionForm initialFormData={initialFormData}/>;
 }
