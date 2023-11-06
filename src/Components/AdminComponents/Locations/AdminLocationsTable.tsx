e2c71257e59a65ed6973df0c54f61fcb375c83d0
michqiueli <michiqueli@gmail.com>
2023-11-06 13:15:33 -0300

    Locations lista

--- src/Components/AdminComponents/Locations/AdminLocationsTable.tsx
+++ src/Components/AdminComponents/Locations/AdminLocationsTable.tsx
@@ -1,10 +1,32 @@
+"use client";
+import { LocationInterface } from "@/src/interfaces";
+import getAllLocations from "@/src/requests/getAlllLocations";
+import { locationsColumns } from "@/src/tableColumns";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getSortedRowModel,
+} from "@tanstack/react-table";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import getAllLocations from "@/src/requests/getAlllLocations";
-import { LocationInterface } from "@/src/interfaces";
 
 export default function AdminLocationsTable() {
   const [locations, setLocations] = useState<LocationInterface[]>([]);
+  const [sorting, setSorting] = useState([]);
+
+  const table = useReactTable({
+    data: locations,
+    columns: locationsColumns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+  });
 
   useEffect(() => {
     async function fetchData() {
@@ -17,45 +39,83 @@ export default function AdminLocationsTable() {
     }
     fetchData();
   }, []);
+
   const router = useRouter();
 
   return (
-    <main className="h-full w-full m-4 justify-center">
-      <div className="mb-4 justify-center">
-        <input placeholder=" Search" />
-        <button>Search</button>
-      </div>
-      <div>
+    <div className=" w-full h-full">
+      <div className="h-4/6">
         <table className="min-w-full rounded-lg w-full h-full bg-slate-700">
           <thead>
-            <tr className="bg-gray-100">
-              <th className="border-b p-2 border-r-2">ID</th>
-              <th className="border-b p-2 border-r-2">Ciudad</th>
-              <th className="border-b p-2 border-r-2">Pais</th>
-              <th className="border-b p-2 border-r-2">Editar</th>
-            </tr>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr
+                key={headerGroup.id}
+                className="border-slate-300 bg-black text-2xl text-white border-solid border"
+              >
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="border-slate-300 border-solid border"
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+
+                        {
+                          { asc: "⬆️", desc: "⬇️" }[
+                            header.column.getIsSorted() ?? null
+                          ]
+                        }
+                      </div>
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
           </thead>
           <tbody>
-            {locations.map((location) => (
-              <tr key={location.id}>
-                <td className="border-b p-2 border-r-2">{location.id}</td>
-                <td className="border-b p-2 border-r-2">{location.city}</td>
-                <td className="border-b p-2 border-r-2">{location.country}</td>
-                <td className="border-b p-2 border-r-2 text-center">
-                  <button
-                    className=" border-red-600 border-solid border-2 bg-green-200 w-32 h-8 rounded"
-                    onClick={() =>
-                      router.push("/AdminDashboard/AdminLocations/EditLocation")
-                    }
+            {table.getRowModel().rows.map((row, index) => (
+              <tr
+                key={row.id}
+                className={index % 2 === 0 ? "bg-slate-700" : "bg-slate-500"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    className="border-slate-300 border-solid border text-white"
                   >
-                    Editar
-                  </button>
-                </td>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-    </main>
+      <div className="h-1/6 w-full flex justify-evenly bg-black mt-2 rounded-lg">
+        <button className="text-2xl" onClick={() => table.setPageIndex(0)}>
+          Primera
+        </button>
+        <button className="text-4xl" onClick={() => table.previousPage()}>
+          ⬅️
+        </button>
+        <span className="text-2xl">
+          {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}
+        </span>
+        <button className="text-4xl" onClick={() => table.nextPage()}>
+          ➡️
+        </button>
+        <button
+          className="text-2xl"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        >
+          Ultima
+        </button>
+      </div>
+    </div>
   );
 }
---
+++ src/tableColumns.tsx
@@ -0,0 +1,17 @@
+
+
+
+export const locationsColumns = [
+  {
+    accessorKey:"id",
+    header: "ID",
+  },
+  {
+    accessorKey: 'city',
+    header: "Ciudad",
+  },
+  {
+    accessorKey: 'country',
+    header: "Pais",
+  }
+];
