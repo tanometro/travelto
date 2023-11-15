48c235b61813a38bde116104617e0f0940db05be
ricardo <rblazquez111@gmail.com>
2023-11-15 15:00:47 -0300

    Cambios de error .map

--- src/Components/Account/UserInfoPanel.tsx
+++ src/Components/Account/UserInfoPanel.tsx
@@ -1,8 +1,25 @@
 "use client"
 
+import { useSession } from "next-auth/react"
+import Image from "next/image";
+
+
+
 function UserInfoPanel() {
+  const { data: session, status } = useSession();
   return (
-    <div>UserInfoPanel</div>
+    <section className="h-full w-full flex flex-col justify-around">
+      <h1>{session?.user.name}</h1>
+      <div className="flex flex-wrap w-300 h-600 bg-white">
+        <Image
+          src={session?.user?.picture!}
+          alt="UserImage"
+          width={300}
+          height={300}
+        />
+
+      </div>
+    </section>
   )
 }
 
