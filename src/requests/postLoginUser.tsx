b5a5d52cc33bad13019373517035132d5ea3110b
michqiueli <michiqueli@gmail.com>
2023-11-14 23:28:35 -0300

    login

--- src/requests/postLoginUser.tsx
+++ src/requests/postLoginUser.tsx
@@ -1,18 +1,35 @@
 import { baseURL } from "@/constant";
+import jwt from "jsonwebtoken";
+const secretKey = 'Dracarys'
 
+import axios from "axios";
 
-const userLogin = async (Credential) => {
-    const response = await fetch(`${baseURL}/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            //Authorization: storedToken,
-        },
-        body: JSON.stringify(Credential),
-    });
-    console.log(response);
+const userLogin = async (credentials) => {
+    const { email, password, googlePass } = credentials;
 
-    return await response.json();
-};
+    // Realizar la solicitud POST de autenticación
+    try {
+        // Generar el token con jsonwebtoken
+        const token = jwt.sign(
+            {
+                email,
+                password,
+                googlePass,
+            }, secretKey, { expiresIn: '1h' })
 
-export default userLogin;
+        // Realizar la solicitud POST de autenticación
+        const response = await axios.post(`${baseURL}/login`, credentials, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        // Devolver la respuesta de la autenticación
+        return response.data;
+    } catch (error) {
+        // Manejar errores de solicitud
+        throw new Error(error);
+    }
+}
+
+export default userLogin;
